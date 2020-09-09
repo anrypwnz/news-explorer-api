9de841fb95c9bbe704b89f96b323c34febb85ba0
anrypwnz <anrypwnz@yandex.ru>
2020-09-09 15:45:05 +0300

    fix: cors

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ const corsOptions = {
   origin: ['http://localhost:8080', 'http://84.201.134.251', 'https://84.201.134.251',
     'https://top-news.ml', 'http://top-news.ml', 'https://www.top-news.ml',
     'http://www.top-news.ml', 'https://api.top-news.ml', 'http://api.top-news.ml',
-    'https://www.api.top-news.ml', 'http://www.api.top-news.ml', 'https://anrypwnz.github.io/news-explorer-frontend/',
-    'http://anrypwnz.github.io/news-explorer-frontend/'],
+    'https://www.api.top-news.ml', 'http://www.api.top-news.ml', 'https://anrypwnz.github.io/', 'http://anrypwnz.github.io/'],
   credentials: true,
   methods: 'GET, POST, DELETE',
 };
